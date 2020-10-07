7129277ab24ed359db197a1e23c31cd73a0bad05
Kinga <niu.ula.a@poczta.fm>
2020-10-07 11:13:09 +0200

    cwiczenie1

--- cwiczenie1.sql
+++ cwiczenie1.sql
@@ -1,8 +1,12 @@
+------------- zadanie 1,2,3
 CREATE DATABASE s301399;
+\c s301399
+
 CREATE SCHEMA firma; 
 CREATE ROLE ksiegowosc;
 GRANT USAGE ON SCHEMA firma TO ksiegowosc;
 
+------------- zadanie 4
 CREATE TABLE firma.pracownicy( id_pracownika INT NOT NULL, imie TEXT, nazwisko TEXT, adres TEXT, telefon TEXT);
 
 CREATE TABLE firma.godziny( id_godziny INT NOT NULL, data DATE, liczba_godzin INT, id_pracownika INT NOT NULL);
@@ -25,9 +29,26 @@ ALTER TABLE firma.wynagrodzenie ADD FOREIGN KEY(id_godziny) REFERENCES firma.god
 ALTER TABLE firma.wynagrodzenie ADD FOREIGN KEY(id_pensji) REFERENCES firma.pensja_stanowisko(id_pensji);
 ALTER TABLE firma.wynagrodzenie ADD FOREIGN KEY(id_premii) REFERENCES firma.premia(id_premii);
 
---------------- przykladowe inserty:
+CREATE UNIQUE INDEX pracownicy_idx ON firma.pracownicy USING btree(id_pracownika);
+
+COMMENT ON TABLE firma.pracownicy IS 'dane pracownikow';
+COMMENT ON TABLE firma.godziny IS 'liczba godzin';
+COMMENT ON TABLE firma.pensja_stanowisko IS 'stanowisko oraz zarobki';
+COMMENT ON TABLE firma.premia IS 'przyznane premie';
+COMMENT ON TABLE firma.wynagrodzenie IS 'wszystko';
+
+ALTER TABLE firma.pracownicy DISABLE TRIGGER ALL;
+ALTER TABLE firma.godziny DISABLE TRIGGER ALL;
+ALTER TABLE firma.wynagrodzenie DISABLE TRIGGER ALL;
+ALTER TABLE firma.pensja_stanowisko DISABLE TRIGGER ALL;
+ALTER TABLE firma.premia DISABLE TRIGGER ALL;
+
+------------- zadanie 5
+ALTER TABLE firma.godziny ADD COLUMN miesiac INT;
+ALTER TABLE firma.godziny ADD COLUMN tydzien INT;
+
 INSERT INTO firma.pracownicy VALUES ( 1, 'Jan', 'Kowalski', 'ul.Kwiatowa16', '453453453');
-INSERT INTO firma.godziny VALUES( 1, '2000-01-01', 100, 1);
+INSERT INTO firma.godziny VALUES( 1, '2000-01-01', 100, 1, 8, 12);
 INSERT INTO firma.pensja_stanowisko VALUES(1, 'informatyk', 15000);
 INSERT INTO firma.premia VALUES(1, 'rodzaj premii', 200000);
 INSERT INTO firma.wynagrodzenie VALUES(1, '2000-01-01', 1, 1, 1, 1);
@@ -42,6 +63,7 @@ SELECT id_pracownika FROM firma.pracownicy WHERE imie LIKE 'J%';
 SELECT id_pracownika FROM firma.pracownicy WHERE imie LIKE '%a' AND nazwisko LIKE '%n%';
 SELECT firma.pracownicy.imie, firma.pracownicy.nazwisko, firma.godziny.liczba_godzin FROM firma.pracownicy, firma.godziny WHERE firma.pracownicy.id_pracownika = firma.godziny.id_godziny;
 SELECT imie, nazwisko FROM firma.pracownicy WHERE id_pracownika = (SELECT id_pracownika FROM firma.wynagrodzenie WHERE id_pensji = ( SELECT id_pensji FROM firma.pensja_stanowisko WHERE kwota>1500 AND kwota<3000 )) ;
+SELECT imie, nazwisko FROM firma.pracownicy JOIN firma.wynagrodzenie ON firma.wynagrodzenie.id_pracownika=firma.pracownicy.id_pracownika JOIN firma.godziny ON firma.godziny.id_godziny=firma.wynagrodzenie.id_godziny JOIN firma.premia ON firma.premia.id_premii=firma.wynagrodzenie.id_premii WHERE liczba_godzin > 160 AND rodzaj LIKE 'brak';
 
 
 ----------------zadanie 7
@@ -55,15 +77,16 @@ SELECT SUM(kwota) FROM firma.pensja_stanowisko;
 SELECT stanowisko, SUM(kwota) FROM firma.pensja_stanowisko GROUP BY stanowisko;
 SELECT stanowisko, COUNT(firma.wynagrodzenie.id_premii) FROM firma.wynagrodzenie JOIN firma.pensja_stanowisko ON firma.pensja_stanowisko.id_pensji=firma.pensja_stanowisko.id_pensji GROUP BY stanowisko;
 
-//DELETE FROM firma.pracownicy JOIN firma.wynagrodzenie ON firma.wynagrodzenie.id_pracownika=firma.pracownicy.id_pracownika JOIN firma.pensja_stanowisko ON firma.stanowisko.id_pensji=firma.wynagrodzenie.id_pensji WHERE kwota<1200;
+DELETE FROM firma.pracownicy WHERE firma.pracownicy.id_pracownika IN (SELECT firma.pracownicy.id_pracownika FROM firma.pracownicy JOIN firma.wynagrodzenie ON firma.wynagrodzenie.id_pracownika = firma.pracownicy.id_pracownika JOIN firma.pensja_stanowisko ON firma.pensja_stanowisko.id_pensji = firma.wynagrodzenie.id_pensji WHERE kwota < 1200);
 
 ----------------zadanie 8
 SELECT '+48' || telefon FROM firma.pracownicy;
+SELECT LEFT(telefon, 3)||'-'||SUBSTRING(telefon, 4, 3)||'-'||RIGHT(telefon,3) FROM firma.pracownicy;
 SELECT UPPER(imie), UPPER(nazwisko) FROM firma.pracownicy ORDER BY LENGTH(nazwisko) DESC LIMIT 1 ;
 SELECT MD5(imie||nazwisko||adres||telefon||pensja_stanowisko.kwota) FROM firma.pracownicy JOIN firma.wynagrodzenie ON firma.pracownicy.id_pracownika=firma.wynagrodzenie.id_pracownika  JOIN firma.pensja_stanowisko ON firma.pensja_stanowisko.id_pensji=firma.pensja_stanowisko.id_pensji;
 
 ----------------zadanie 9 - RAPORT 
-SELECT 'Pracownik '||imie||' '||nazwisko||' w dniu '||data||' otrzymal pensje calkowita na kwote '||firma.pensja_stanowisko.kwota||' gdzie wynagrodzenie to '||firma.stanowisko_pensja.kwota-firma.premia.kwota||' premia: '||firma.premia.kwota RAPORT FROM firma.pracownicy JOIN firma.wynagrodzenie ON firma.pracownicy.id_pracownika=firma.wynagrodzenie.id_pracownika  JOIN firma.pensja_stanowisko ON firma.pensja_stanowisko.id_pensji=firma.pensja_stanowisko.id_pensji JOIN firma.premia ON firma.premia.id_premii = firma.wynagrodzenie.id_premii;
+SELECT 'Pracownik '||imie||' '||nazwisko||' w dniu '||wynagrodzenie.data||' otrzymal pensje calkowita na kwote '||firma.pensja_stanowisko.kwota||' gdzie wynagrodzenie zasadnicze to '||firma.pensja_stanowisko.kwota-firma.premia.kwota||' premia: '||firma.premia.kwota||' nadgodziny: '||godziny.liczba_godzin-160 RAPORT FROM firma.pracownicy JOIN firma.wynagrodzenie ON firma.pracownicy.id_pracownika=firma.wynagrodzenie.id_pracownika  JOIN firma.pensja_stanowisko ON firma.pensja_stanowisko.id_pensji=firma.wynagrodzenie.id_pensji JOIN firma.premia ON firma.premia.id_premii = firma.wynagrodzenie.id_premii JOIN firma.godziny ON firma.wynagrodzenie.id_godziny=firma.godziny.id_godziny;
 
 
 
